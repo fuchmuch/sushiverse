dba86282eb261edc4da00df66a9e1f3ead202954
“Gihae <designbygihae@gmail.com>
2021-12-22 11:46:59 -0500

    update

--- frontend/src/components/List/PlaceCard.jsx
+++ frontend/src/components/List/PlaceCard.jsx
@@ -6,7 +6,7 @@ export default function PlaceCard(props) {
   return (
     <div className='place-card' onClick={props.onClick}>
       <section className="place-info">
-        <h1>{props.name}</h1>
+        <h1 className='eachnameplace'>{props.name}</h1>
         <img src={props.image} />
       </section>
     </div>
