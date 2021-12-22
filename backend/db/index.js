56c80342b0eaf2fa8a4d946a54f95e770c4f73d0
“Gihae <designbygihae@gmail.com>
2021-12-22 14:40:00 -0500

    heroku

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/placesDatabase'
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/placesDatabase'
 
 mongoose
-    .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+    .connect(dbUrl)
     .then(() => {
         console.log('Successfully connected to MongoDB.')
     })
@@ -11,8 +12,7 @@ mongoose
         console.error('Connection error', e.message)
     })
 
-// mongoose.set('debug', true)
 const db = mongoose.connection
-
+// mongoose.set('debug', true)
 
 module.exports = db
--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,17 @@ app.use(logger('dev'))
 
 app.use('/api', routes);
 
+
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')))
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+    })
+  }
+
+
+
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
